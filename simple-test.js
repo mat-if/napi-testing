b11740fc07d16afe6a4c8c0e9f82c8dcd46bf8ae
Mat <mat@ironfish.network>
2022-08-23 10:18:36 -0700

    Simple demo

--- simple-test.js
+++ simple-test.js
@@ -1,4 +1,72 @@
-const { plus100 } = require('./index')
+const { getBuffer, plus100, EmptyClass } = require('./index')
+
+class JsEmptyClass {}
+
+function runGc() {
+  for (let i = 0; i < 5; i++) {
+    global.gc()
+  }
+}
+
+async function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+async function sleepAndGc(ms) {
+  await sleep(ms)
+  runGc()
+}
+
+async function testClass(name, cls) {
+  const startMem = process.memoryUsage()
+
+  for (let i = 0; i < 1_000_000; i++) {
+    const x = new cls()
+
+    if (i % 5000 === 0) {
+      await sleep(10)
+    }
+  }
+
+  const endMem = process.memoryUsage()
+
+  const heapDiff = endMem.heapUsed - startMem.heapUsed
+  const rssDiff = endMem.rss - startMem.rss
+  console.log(name)
+  console.log('Heap diff:', heapDiff.toLocaleString())
+  console.log('Rss diff:', rssDiff.toLocaleString())
+  console.log('')
+}
+
+async function testBuffer() {
+  const startMem = process.memoryUsage()
+
+  for (let i = 0; i < 1_000_000; i++) {
+    const x = getBuffer()
+
+    if (i % 5000 === 0) {
+      await sleep(10)
+    }
+  }
+
+  const endMem = process.memoryUsage()
+
+  const heapDiff = endMem.heapUsed - startMem.heapUsed
+  const rssDiff = endMem.rss - startMem.rss
+  console.log('getBuffer')
+  console.log('Heap diff:', heapDiff.toLocaleString())
+  console.log('Rss diff:', rssDiff.toLocaleString())
+  console.log('')
+}
+
+// JS class
+// void testClass('JS', JsEmptyClass)
+
+// Rust class
+void testClass('Rust', EmptyClass)
+
+// Test buffer
+// void testBuffer()
 
 console.assert(plus100(0) === 100, 'Simple test failed')
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,15 @@
 #![deny(clippy::all)]
 
+use napi::bindgen_prelude::Buffer;
 use napi_derive::napi;
+
+#[napi(constructor)]
+pub struct EmptyClass {}
+
+#[napi]
+pub fn get_buffer() -> Buffer {
+  vec![1u8; 32].into()
+}
 
 #[napi]
 pub fn plus_100(input: u32) -> u32 {
